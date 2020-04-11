fb71e8b044f8cc58c3073d16107a39de63a19cf4
BENMALEKarim <fk_benmalek@esi.dz>
2020-04-11 16:47:15 +0100

    update resolve functions to interact with mongodb

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,6 +19,7 @@ const BookType = new GraphQLObjectType({
             type: AuthorType,
             resolve(parent, args){
                 //return _.find(authors, {id: parent.authorId});
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -34,6 +35,7 @@ const AuthorType = new GraphQLObjectType({
             type: GraphQLList(BookType),
             resolve(parent, args){
                 //return _.filter(books, {authorId: parent.id});
+                return Book.find({authorId: parent.id});
             }
         }
     })
@@ -47,13 +49,15 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //return _.find(books, { id: args.id });
+                return Book.findById(args.id);
             }
         },
         author: {
             type: AuthorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return _.find(authors, { id: args.id });
+                //return _.find(authors, { id: args.id });
+                return Author.findById(args.id);
             }
         },
 
@@ -61,12 +65,14 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent, args){
                 //return books;
+                return Book.find({});
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
                 //return authors;
+                return Author.find({});
             }
         }
     }
@@ -87,6 +93,22 @@ const Mutation = new GraphQLObjectType({
                     age: args.age
                 });
                 return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+             resolve(parent, args) {
+                let book = Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId,
+                });
+                return book.save();
             }
         }
     }
