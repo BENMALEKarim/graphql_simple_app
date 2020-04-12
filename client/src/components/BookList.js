01f531b7d26261952c9a64f769deb8e2ff003537
BENMALEKarim <fk_benmalek@esi.dz>
2020-04-12 12:39:11 +0100

    Updating component state

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import {graphql} from 'react-apollo';
+import { getBooksQuery } from '../queries/queries';
+
 
 
 class BookList extends Component {
+    displayBooks(){
+        var data = this.props.data;
+        if (data.loading) {
+            return (<div>Loading books...</div>);
+        } else {
+            return data.books.map(book => {
+                return (
+                    <li key={book.id}>{book.name}</li>
+                );
+            })
+        }
+    }
+
     render(){
         return (
             <div>
                 <ul id="book-list">
-                    <li>Book-name</li>
+                    {this.displayBooks()}
 
                 </ul>
             </div>
@@ -14,4 +30,4 @@ class BookList extends Component {
     }  
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
